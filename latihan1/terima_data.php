fb09aa11bdbe206890f36f054c4bdc26955a1f18
FADHIL PRAWIRA <fadhilprawira87@gmail.com>
2023-04-08 12:55:30 +0700

    update

---
+++ latihan1/terima_data.php
@@ -0,0 +1,12 @@
+<?php
+
+echo "Nama saya ".$_GET["nama"];
+echo "<br>";
+
+echo "Matkul 1 yang saya ambil adalah:".$_GET["matkul1"];
+echo "<br>";
+echo "Matkul 2 yang saya ambil adalah:".$_GET["matkul2"];
+echo "<br>";
+echo "Matkul 3 yang saya ambil adalah:".$_GET["matkul3"];
+echo "<br>";
+echo "Password saya adalah:".$_GET["myPass"];
